3f9ab21ae9568caf4223a9bcbdeb7f623e09e0f7
Pawel Sudol <piotr.pawelsudol@stryker.com>
2019-01-30 15:05:49 -0500

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/s-calendar'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/s-calendar/index.html'));
+app.get('/*all', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
